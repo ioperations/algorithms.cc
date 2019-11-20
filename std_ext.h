1555cbe761971e275803db9b597fe5557920cf6e
Nikita Karatun <nikitakhv88@gmail.com>
2019-11-20 15:20:57 +0300

    rich text: styled enties: in progress

---
+++ std_ext.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <array>
+
+namespace Std_ext {
+
+    template<typename T, std::size_t SZ, std::size_t I = 0>
+        inline void fill_array(std::array<T, SZ>& array) {
+        }
+
+    template<typename T, std::size_t SZ, std::size_t I = 0, typename... Args>
+        inline void fill_array(std::array<T, SZ>& array, T&& t, Args&&... args) {
+            array[I] = t;
+            fill_array<T, SZ, I + 1>(array, std::forward<Args>(args)...);
+        }
+
+    template<typename T, typename... Args, std::size_t SZ = sizeof...(Args) + 1>
+        inline auto make_array(T&& t, Args&&... args) {
+            std::array<T, SZ> array;
+            fill_array<T, SZ>(array, std::forward<T>(t), std::forward<Args>(args)...);
+            return array;
+        }
+
+    template<typename T, std::size_t SZ, std::size_t I = 0, typename F>
+        inline typename std::enable_if<I == SZ, void>::type for_each(const std::array<T, SZ>& array, F f) {
+        }
+
+    template<typename T, std::size_t SZ, std::size_t I = 0, typename F>
+        inline typename std::enable_if<I < SZ, void>::type for_each(const std::array<T, SZ>& array, F f) {
+            f(array[I]);
+            for_each<T, SZ, I + 1>(array, f);
+        }
+
+}
---
+++ tests/std_ext_test.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+#include <sstream>
+
+#include "std_ext.h"
+
+TEST(Std_ext_test, std_array) {
+    auto a = Std_ext::make_array(1, 2, 3);
+    std::stringstream ss;
+    ss << a[0] << " " << a[1] << " " << a[2];
+    ASSERT_EQ(ss.str(), "1 2 3");
+
+    ss = {};
+    Std_ext::for_each(a, [&ss](auto& i) { ss << i << " "; });
+    ASSERT_EQ(ss.str(), "1 2 3 ");
+}
