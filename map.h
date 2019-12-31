6293d8a756005a820c5a1190ae965983dc2ed754
Nikita Karatun <nikitakhv88@gmail.com>
2019-12-31 15:50:31 +0300

    map: insertion and iteration added

--- map.h
+++ map.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "binary_tree.h"
+#include "stack.h"
 
 #include <iostream>
 
@@ -14,16 +15,46 @@ class Map {
         using node_type = Binary_tree_node<Node>;
     private:
         template<typename KK, typename VV>
-        friend class Map_printer;
+            friend class Map_printer;
         node_type* root_;
     public:
         Map() :root_(nullptr) {}
         ~Map() { delete root_; }
         void insert(const K& key, const V& value) {
-            if (root_) {
-            } else {
-                root_ = new Binary_tree_node<Node>({key, value});
+            if (!root_) {
+                root_ = new node_type({key, value});
+                return;
             }
+            auto node = root_;
+            bool found = false;
+            for (auto next = node; next && !found; ) {
+                if (key == node->value_.key_) {
+                    node->value_.value_ = value;
+                    found = true;
+                } else {
+                    next = key < node->value_.key_ ? node->l_ : node->r_;
+                    if (next)
+                        node = next;
+                }
+            }
+            if (!found) {
+                if (key < node->value_.key_)
+                    node->l_ = new node_type({key, value});
+                else
+                    node->r_ = new node_type({key, value});
+            }
+        }
+
+        void iterate(node_type* node) {
+            if (!node)
+                return;
+            iterate(node->l_);
+            std::cout << node->value_.key_ << std::endl;
+            iterate(node->r_);
+        }
+
+        void iterate() {
+            iterate(root_);
         }
 };
 
@@ -32,7 +63,7 @@ class Map_printer_node_handler : public Binary_tree_printer_node_handler<typenam
     public:
         std::string node_to_string(const typename Map<K, V>::node_type& n) {
             std::stringstream ss;
-            ss << "{" << n.value().key_ << ": " << n.value().value_ << "}";
+            ss << n.value().key_ << ":" << n.value().value_;
             return ss.str();
         }
 };
@@ -46,4 +77,4 @@ class Map_printer :
             void print(const Map<K, V>& map, std::ostream& stream) {
                 Base::print(*map.root_, stream);
             }
-};
+    };
--- tests/binary_tree_test.cpp
+++ tests/binary_tree_test.cpp
@@ -4,8 +4,18 @@
 
 TEST(Binary_tree, binary_tree) {
     Map<int, int> map;
-    map.insert(1, 1);
+    map.insert(10, 1);
+    map.insert(20, 2);
+    map.insert(50, 3);
+    map.insert(40, 4);
+    map.insert(5, 5);
+    map.insert(21, 6);
+    map.insert(21, 7);
 
     Map_printer<int, int> printer;
     printer.print(map, std::cout);
+
+    std::cout << std::endl;
+
+    map.iterate();
 }
